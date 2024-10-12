f0522205aafb5179d5b899341dad737f10d0f3ca
Ashwanth S <appleashwanth2004@gmail.com>
2024-10-12 08:31:00 +0530

    Initial Commit

---
+++ src/AirlineManagementSystem/Database.java
@@ -0,0 +1,62 @@
+package AirlineManagementSystem;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class Database {
+
+    private  String url="jdbc:mysql://localhost/airline management system";
+    private  String user ="user";
+    private  String pass ="lakshmivasan74";
+
+    private Statement statement;
+
+    public Database() throws SQLException
+    {
+      Connection connection= DriverManager.getConnection(url,user,pass);
+      statement=connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+    }
+
+
+    public void AddPassenger(Passenger p) throws SQLException {
+        String insert="INSERT INTO `passengers`(`id`, `firstName`, `lastName`," +
+                " `Tel`, `email`) VALUES ('"+p.getId()+"','"+p.getFirstName()+"','"+p.getLastName()+"','"+p.getTel()+"','"+p.getEmail()+"')";
+        statement.execute(insert);
+    }
+
+    public Passenger getPassenger(int id) throws SQLException {
+        String get ="SELECT `id`, `firstName`, `lastName`, `Tel`, `email` FROM `passengers` WHERE `id` = "+id+";";
+        ResultSet rs =  statement.executeQuery(get);
+
+        Passenger p=new Passenger();
+        p.setId(Integer.parseInt(rs.getString("id")));
+        p.setFirstName(rs.getString("firstName"));
+        p.setLastName(rs.getString("lastName"));
+        p.setTel(rs.getString("Tel"));
+        p.setEmail(rs.getString("email"));
+        return p;
+    }
+
+    public ArrayList<Passenger> getAllPassengers() throws SQLException
+    {
+        String get ="SELECT * FROM `passengers`;";
+      ResultSet rs =  statement.executeQuery(get);
+      ArrayList<Passenger>passengers = new ArrayList<>();
+      while(rs.next())
+      {
+           Passenger p=new Passenger();
+           p.setId(Integer.parseInt(rs.getString("id")));
+           p.setFirstName(rs.getString("firstName"));
+           p.setLastName(rs.getString("lastName"));
+           p.setTel(rs.getString("Tel"));
+           p.setEmail(rs.getString("email"));
+           passengers.add(p);
+
+      }
+      return  passengers;
+    }
+    public Statement getStatement() {
+        return statement;
+    }
+
+}
